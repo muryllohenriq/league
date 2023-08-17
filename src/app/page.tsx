028aeeb601cc3dc1ab90f2de185872c14bbf44e2
Muryllo <muryllo.henrique@hotmail.com>
2023-08-16 21:49:49 -0300

    Imaginando o produto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Navbar } from "@/components/navbar"
+
 export default function Home() {
-  const matchUps = ['Neeko - Flash Ignite', 'Fizz - Flash Ghost']
+  const matchUps = ['Aatrox', 'Fizz - Flash Ghost', 'Neeko - Flash Ignite', 'Xerath - Flash Ghost']
 
   return (
     <main className="">
+      <Navbar/>
       <h1>Vladimir</h1>
       <ul>
         {matchUps.map((e) => <li>Vladimir x {e}</li>)}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export function Navbar() {
+    return (
+        <ul className="flex">
+            <li>Inicio</li>
+            <li>Matchups</li>
+            <li>Indicações</li>
+        </ul>
+    )
+}
