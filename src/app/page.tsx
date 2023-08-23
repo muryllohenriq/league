f1092ea1e6dfe0e47e0b128ce7f2473e3a6a6247
Muryllo <muryllo.henrique@hotmail.com>
2023-08-22 22:30:40 -0300

    Imaginando a aplicação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
-import { Navbar } from "@/components/Navbar"
-
 export default function Home() {
-  const matchUps = ['Aatrox - ', 'Ahri - ', 'Akali - ', 'Akshan - ', 'Anivia - ', 'Annie - ', 'Aurelion Sol - ', 'Azir - ', 'Brand - ', 'Camille - ', 'Cassiopeia - ', 'Cho Gath - ', 'Corki - ', 'Darius - ', 'Diana - ', 'Dr. Mundo - ', 'Ekko - ', 'Fiora - ', 'Fizz - Flash Ghost', 'Galio - ', 'Gangplank - ', 'Garen - ', 'Gnar - ', 'Gwen - ', 'Heimerdinger - ', 'Illaoi - ', 'Irelia - ', 'Jax - ', 'Jayce - ', 'K Sante - ', 'Karma - ', 'Kassadin - ', 'Katarina - ', 'Kayle - ', 'Kennen - ', 'Kled - ', 'Leblanc - ', 'Lissandra - ', 'Lucian - ', 'Lux - ', 'Malphite - ', 'Malzahar - ', 'Mordekaiser - ', 'Morgana - ', 'Nasus - ', 'Neeko - Flash Ignite', 'Olaf - ', 'Orianna - ', 'Ornn - ', 'Pantheon - ', 'Poppy - ', 'Qiyana - ', 'Quinn - ', 'Renekton - ', 'Rengar - ', 'Riven - ', 'Rumble - ', 'Ryze - ', 'Seraphine - ', 'Sett - ', 'Shen - ', 'Singed - ', 'Sion - ', 'Swain - ', 'Sylas - ', 'Syndra - ', 'Tahm Kench - ', 'Taliyah - ', 'Talon - ', 'Teemo - ', 'Tristana - ', 'Trundle - ', 'Tryndamere - ', 'Twisted Fate - ', 'Urgot - ', 'Veigar - ', 'Vel Koz - ', 'Vex - ', 'Viktor - ', 'Volibear - ', 'Wukong - ', 'Xerath - Flash Ghost', 'Yasuo - ', 'Yone - ', 'Yorick - ', 'Zed - ', 'Ziggs - ', 'Zilean - ', 'Zoe - ']
+  const matchUps = ['Aatrox', 'Ahri', 'Akali', 'Akshan', 'Anivia', 'Annie', 'Aurelion Sol', 'Azir', 'Brand', 'Camille', 'Cassiopeia', 'Cho Gath', 'Corki', 'Darius', 'Diana', 'Dr. Mundo', 'Ekko', 'Fiora', 'FizzFlash Ghost', 'Galio', 'Gangplank', 'Garen', 'Gnar', 'Gwen', 'Heimerdinger', 'Illaoi', 'Irelia', 'Jax', 'Jayce', 'K Sante', 'Karma', 'Kassadin', 'Katarina', 'Kayle', 'Kennen', 'Kled', 'Leblanc', 'Lissandra', 'Lucian', 'Lux', 'Malphite', 'Malzahar', 'Mordekaiser', 'Morgana', 'Nasus', 'NeekoFlash Ignite', 'Olaf', 'Orianna', 'Ornn', 'Pantheon', 'Poppy', 'Qiyana', 'Quinn', 'Renekton', 'Rengar', 'Riven', 'Rumble', 'Ryze', 'Seraphine', 'Sett', 'Shen', 'Singed', 'Sion', 'Swain', 'Sylas', 'Syndra', 'Tahm Kench', 'Taliyah', 'Talon', 'Teemo', 'Tristana', 'Trundle', 'Tryndamere', 'Twisted Fate', 'Urgot', 'Veigar', 'Vel Koz', 'Vex', 'Viktor', 'Volibear', 'Wukong', 'XerathFlash Ghost', 'Yasuo', 'Yone', 'Yorick', 'Zed', 'Ziggs', 'Zilean', 'Zoe']
 
   return (
     <main className="">
-      <Navbar/>
-      <h1>Vladimir</h1>
-      <ul>
-        {matchUps.map((e) => <li>{e}</li>)}
-      </ul>
+      <h1 className="flex justify-center">Vladimir</h1>
+      <div className="">
+      {matchUps.map((e) => <div className="border w-28">{e}</div>)}
+      </div>
     </main>
   )
 }
--- src/components/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-export function Navbar() {
-    return (
-        <ul className="flex gap-2">
-            <li>Inicio</li>
-            <li>Matchups</li>
-            <li>Indicações</li>
-        </ul>
-    )
-}
